37c8ab89bf2d60c7f2cb6e0c80452b073ae96a83
manas.jain <manas.jain@grabtaxi.com>
2021-01-23 14:13:29 +0530

    adds upcoming event

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,11 +27,6 @@
 })(jQuery,'smartresize');
 
 
-
-
-
-
-
 (function(){
 
 	$wrapper = $('#wrapper');
@@ -134,3 +129,45 @@
 	});
 
 })();
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+		event.preventDefault();
+		
+		// Get the height of the header
+		var headerHeight = $("div#header").height();
+		var scrollToPosition = $(target).offset().top - 100;
+		
+        $('html, body').animate({
+          scrollTop: scrollToPosition 
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
